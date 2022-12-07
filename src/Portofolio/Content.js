ac2ad22dfb46eaa5ad4f362fa605f4197a4ec1c1
Aliva <alivanurramadhan@gmail.com>
2022-12-07 22:50:56 +0800

    memambahkan responsive ipad

--- src/Portofolio/Content.js
+++ src/Portofolio/Content.js
@@ -52,7 +52,7 @@ export default function Content() {
 
       <div className="row">
         <div className="col-sm-5">
-          <div className="content-mobile d-md-block d-lg-none">
+          <div className="content-mobile d-sm-block d-md-none">
             <h2 className="mt-3">Full Stack Developer</h2>
             <h3 className="name">Muhammad Aliva Nurramadhan</h3>
             <hr className="col-sm-3 garis" color="white"></hr>
@@ -66,7 +66,7 @@ export default function Content() {
         <div className="col-sm-5 text-center">
           <img
             src="./AlipTamvan.jpg"
-            className="img-content-mobile d-md-block d-lg-none img-fluid"
+            className="img-content-mobile d-sm-block d-md-none img-fluid"
             alt="Foto Alip"
           />
         </div>
@@ -74,31 +74,76 @@ export default function Content() {
           <div className="container">
             <div className="row">
               <div className="col-4 text-center">
-                <div className="content-mobile d-md-block d-lg-none">
+                <div className="content-mobile  d-sm-block d-md-none">
                   <h5 className="mt-3 social-prove">Year Of Experience</h5>
-                  <h3 className="name d-md-block d-lg-none ">
+                  <h3 className="name  d-sm-block d-md-none ">
                     <b>4+</b>
                   </h3>
                 </div>
               </div>
               <div className="col-4 text-center">
-                <div className="content-mobile d-md-block d-lg-none ">
+                <div className="content-mobile  d-sm-block d-md-none ">
                   <h5 className="mt-3 social-prove">Complete Project</h5>
-                  <h3 className="name d-md-block d-lg-none ">
+                  <h3 className="name  d-sm-block d-md-none">
                     <b>30+</b>
                   </h3>
                 </div>
               </div>
 
               <div className="col-4 text-center">
-                <div className="content-mobile d-md-block d-lg-none">
+                <div className="content-mobile  d-sm-block d-md-none">
                   <h5 className="mt-3 social-prove">Client</h5>
-                  <h3 className="name d-md-block d-lg-none ">
+                  <h3 className="name  d-sm-block d-md-none">
                     <b>17</b>
                   </h3>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Ipad */}
+      <div className="row">
+        <div className="col-sm-5">
+          <div className="content-ipad  d-none d-md-block d-lg-none">
+            <h2 className="mt-3">Full Stack Developer</h2>
+            <h3 className="name">Muhammad Aliva Nurramadhan</h3>
+            <hr className="col-sm-5 garis" color="white"></hr>
+            <h5 className="text-justify">
+              A freelancer who provides service for digital programming and
+              design content needs, for all business with more than 4+
+              Experience
+            </h5>
+          </div>
+        </div>
+        <div className="col-sm-5 text-center">
+          <img
+            src="./AlipTamvan.jpg"
+            className="img-content-ipad  d-none d-md-block d-lg-none img-fluid"
+            alt="Foto Alip"
+          />
+        </div>
+        <div className="col-sm-2">
+          <div className="content-ipad d-none d-md-block d-lg-none text-end">
+            <h5 className="mt-3 social-prove">Year Of Experience</h5>
+            <h3 className="name d-none d-md-block d-lg-none">
+              <b>4+</b>
+            </h3>
+          </div>
+
+          <div className="content-ipad d-none d-md-block d-lg-none text-end">
+            <h5 className="mt-3 social-prove">Complete Project</h5>
+            <h3 className="name d-none d-md-block d-lg-none">
+              <b>30+</b>
+            </h3>
+          </div>
+
+          <div className="content-ipad      d-none d-md-block d-lg-none text-end">
+            <h5 className="mt-3 social-prove">Client</h5>
+            <h3 className="name d-none d-md-block d-lg-none ">
+              <b>17</b>
+            </h3>
           </div>
         </div>
       </div>
